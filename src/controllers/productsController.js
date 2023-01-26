efca2ed8c00612ed86c85db9ffe842a145bc40fb
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-01-26 19:12:23 -0300

    feat: endpoint/products criado

---
+++ src/controllers/productsController.js
@@ -0,0 +1,10 @@
+const productsService = require('../services/productsService');
+
+const getAll = async (req, res) => {
+  const products = await productsService.getAll();
+  res.status(200).json(products);
+};
+
+module.exports = {
+  getAll,
+};
