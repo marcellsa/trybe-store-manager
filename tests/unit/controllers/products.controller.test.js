6e2a842a3ebb30b0a7258b50268c844ed89460ff
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-02-01 16:53:01 -0300

    feat: adicionando testes productsController

--- tests/unit/controllers/products.controller.test.js
+++ tests/unit/controllers/products.controller.test.js
@@ -26,11 +26,33 @@ describe('Products Controller', function () {
       sinon.stub(productsService, 'getProducts').resolves([productsList]);
       
       await productsController.getProducts(req, res);
-      expect(res.status).to.has.been.calledWith(200);
+      expect(res.status).to.have.been.calledWith(200);
       expect(res.json).to.have.been.calledWithExactly([productsList]);
     });
   });
 
-  
+  describe('REQ-02 - Listar produto por ID', function () {
+    const req = {};
+    const res = {};
+
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+    });
+    
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('Deverá retornar a response com status com status 400 e mensagem not found', async function () {
+      req.params = { id: 9 };
+
+      sinon.stub(productsService, 'getProductsById').resolves(false);
+      
+      await productsController.getProductsById(req, res);
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWithExactly({ message: "Product not found" });
+    });
+  });
 
 });
